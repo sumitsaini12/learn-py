3e7182ca6e6f7d61bded5e289faf282e371f8119
subhaminapp <subham.j@inapp.com>
2026-08-18 23:13:31 +0530

    solve if else condition questions

---
+++ ifel-condition.py
@@ -0,0 +1,57 @@
+#1.   find grater number between two numbers using if else statement
+
+# num_1 = int(input("Enter first number:- "));
+# num_2 = int(input("Enter second number:- "))
+
+
+# if num_1 > num_2:
+#     print(f"first number {num_1} is greater than second number {num_2}")
+# elif num_1 == num_2:
+#     print("both numbers are equal")
+# else:
+#     print(f"second number {num_2} is greater than first number {num_1}")
+
+
+
+#2. find even or odd number using if else statement
+
+# num = int(input("Enter a number:- "))
+
+# if num % 2 == 0:
+#     print("this number is a even number");
+# else:
+#     print("this number is a odd number");
+
+
+
+
+
+# 3. Accept name and age from the user. Check if the user is a valid voter or not.
+
+# name = input("Enter your name:- ")
+# age = int(input("Enter your age:- "))
+
+
+# if age >= 18 :
+#     print(f"Hi {name} you are a valid voter")
+# else: 
+#     print(f"Hi {name}, sorry you are not a valid voter")
+
+
+
+
+
+
+#4. Accept a year and check if it is a leap year or not. (Google to find out what a leap year is.)
+
+# year = int(input("Enter your year :- "))
+
+# if year % 400 == 0:
+#     print("This is a leap year");
+# elif year % 100 ==0:
+#     print("This is not a leap year");
+# elif year % 4 ==0:
+#     print("This is a leap year")
+# else:
+#     print("This is not a leap year")
+
