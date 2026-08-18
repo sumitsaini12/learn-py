da612539bccb16843736b16f85b5aca296c17671
sumitsaini12 <sumitsaini95488@gmail.com>
2026-08-18 23:51:00 +0530

    add forLoop.py with multiple number printing functionalities

---
+++ forLoop.py
@@ -0,0 +1,41 @@
+# 1. Accept an integer and print "Hello World" n times.
+
+# number = int(input("Who many time do you want to print hello world:- "));
+
+# for i in range(number):
+#     print(f"{i + 1}. Hello world");
+
+
+
+
+# 2. Print natural numbers up to n
+
+# num_1 = int(input('Enter your number:- '));
+
+# for i in range(1, num_1 + 1):
+#     print(i)
+
+
+
+# 3. Reverse for loop: Print n to 1
+
+# num_1 = int(input("Enter your number:- "));
+
+# for i in range(num_1, 0, -1):
+#     print(i)
+
+
+#4. Take a number as input and print its table
+
+# (i) case 1
+# num_1 = int(input("Which number table do you want:- "))
+
+# for i in range(1, 11):
+#     print(f"{num_1} X {i} = {num_1 * i}")
+
+# (ii) case 2
+# num_1 = int(input("Which number table do you want: "))
+# num_2 = int(input("How long do you want the table: "))
+
+# for i in range(1, num_2 + 1):
+#     print(f"{num_1} X {i} = {num_1 * i}")
